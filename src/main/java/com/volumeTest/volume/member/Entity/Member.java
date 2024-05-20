70326274e6ba97227b0ec6b2805b5a27ff7bc084
Jgone2 <jgoneit@gmail.com>
2024-05-20 14:04:21 +0900

    :sparkles:: KAN-10--Member Member Entity 생성

--- src/main/java/com/volumeTest/volume/member/Entity/Member.java
+++ src/main/java/com/volumeTest/volume/member/Entity/Member.java
@@ -1,2 +1,27 @@
-package com.volumeTest.volume.member.Entity;public class Member {
+package com.volumeTest.volume.member.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "MEMBERS")
+public class Member {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int memberId;
+
+  @Column(nullable = false, unique = true, updatable = false, length = 30)
+  private String email;
+
+  @Column(nullable = false, unique = false, updatable = true, length = 20)
+  private String password;
+
+  @Column(nullable = false, unique = false, updatable = true, length = 20)
+  private String name;
 }
