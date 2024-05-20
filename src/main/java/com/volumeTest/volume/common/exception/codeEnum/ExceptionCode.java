5fbb173fe27c4dcbc95aa33a78c615666218b943
Jgone2 <jgoneit@gmail.com>
2024-05-20 16:15:28 +0900

    :broom:: KAN-10--Member 불필요 Exception Code 제거

--- src/main/java/com/volumeTest/volume/common/exception/codeEnum/ExceptionCode.java
+++ src/main/java/com/volumeTest/volume/common/exception/codeEnum/ExceptionCode.java
@@ -10,9 +10,7 @@ public enum ExceptionCode {
   // Member
   MEMBER_NOT_FOUND(404, 10001, "사용자를 찾을 수 없습니다."),
   MEMBER_EXISTS(409, 10002, "이미 존재하는 사용자입니다."),
-  MEMBER_LOGIN_ID_EXISTS(409, 10003, "이미 존재하는 아이디 입니다."),
-  MEMBER_EMAIL_EXISTS(409, 10004, "이미 존재하는 Email 입니다."),
-  MEMBER_PHONE_NUM_EXISTS(409, 10005, "이미 존재하는 휴대폰 번호 입니다."),
+  MEMBER_EMAIL_EXISTS(409, 10003, "이미 존재하는 Email 입니다."),
 
   // Verify
   MEMBER_PASSWORD_MISMATCH(409, 10201, "비밀번호가 일치하지 않습니다."),
