3971f954c10c7ded276a654107d5e32d38bf4399
Jgone2 <jgoneit@gmail.com>
2024-05-20 15:29:55 +0900

    :sparkles:: KAN-10--Member Dto변환을 위한 MemberMapper Interface 추가

--- src/main/java/com/volumeTest/volume/member/mapper/MemberMapper.java
+++ src/main/java/com/volumeTest/volume/member/mapper/MemberMapper.java
@@ -1,2 +1,22 @@
-package com.volumeTest.volume.member.mapper;public interface MemberMapper {
+package com.volumeTest.volume.member.mapper;
+
+import com.volumeTest.volume.member.dto.MemberDto;
+import com.volumeTest.volume.member.entity.Member;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface MemberMapper {
+  // To Entity
+  Member memberPostDtoToMember(MemberDto.Post memberPostDto);
+  Member memberPatchDtoToMember(MemberDto.Patch memberPatchDto);
+
+  // Login
+  MemberDto.Login memberToLoginDto(Member member);
+
+  // Check
+  MemberDto.CheckPassword checkPasswordToMember(MemberDto.CheckPassword checkPasswordDto);
+
+  // Response
+  MemberDto.MemberPostResponse memberToMemberPostResponseDto(Member member);
+  MemberDto.MemberPatchResponse memberToMemberPatchResponseDto(Member member);
 }
