0ecedf030cafffa27ead572b912e22ea9b630432
twoweekhee <myjjoo4758@icloud.com>
2024-05-20 10:14:44 +0900

    ✨ Create concert
    create concert dto

--- src/main/java/com/volumeTest/volume/dto/ConcertResponseDto.java
+++ src/main/java/com/volumeTest/volume/dto/ConcertResponseDto.java
@@ -3,35 +3,17 @@ package com.volumeTest.volume.dto;
 import com.volumeTest.volume.entity.ConcertEntity;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.Date;
 
+@Getter
 @Builder
-public class ConcertDto {
+public class ConcertResponseDto {
 
     @NotNull
-    private int concertId;
+    private Long concertId;
     private String concertName;
     private Date concertDate;
 
-    public ConcertDto(int concertId, String concertName, Date concertDate) {
-        this.concertId = concertId;
-        this.concertName = concertName;
-        this.concertDate = concertDate;
-    }
-
-    public ConcertEntity toEntity() {
-        return ConcertEntity.builder()
-                .concertName(concertName)
-                .concertDate(concertDate)
-                .build();
-    }
-
-    public ConcertDto toDto(ConcertEntity concert) {
-        return ConcertDto.builder()
-                .concertId(concert.getConcertID())
-                .concertName(concert.getConcertName())
-                .concertDate(concert.getConcertDate())
-                .build();
-    }
 }
