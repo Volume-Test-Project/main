4fc69529dbecdecfd00663a6177b758e386b6e1d
Jgone2 <jgoneit@gmail.com>
2024-05-20 14:04:42 +0900

    :sparkles:: KAN-10--MemberDto 생성

--- src/main/java/com/volumeTest/volume/member/Dto/MemberDto.java
+++ src/main/java/com/volumeTest/volume/member/Dto/MemberDto.java
@@ -1,2 +1,98 @@
-package com.volumeTest.volume.member.Dto;public class MemberDto {
-}
+package com.volumeTest.volume.member.Dto;
+
+import com.volumeTest.volume.common.pattern.custom.member.Email;
+import com.volumeTest.volume.common.pattern.custom.member.Name;
+import com.volumeTest.volume.common.pattern.custom.member.Password;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+public class MemberDto {
+
+  @Getter
+  @Builder
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class Login {
+
+    @Email
+    private String email;
+    @Password
+    private String password;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    public static class Post {
+
+      @Email
+      private String email;
+      @Password
+      private String password;
+      @Name
+      private String name;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    public static class Patch {
+
+      @Password
+      private String password;
+      @Name
+      private String name;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    public static class CheckPassword {
+      @Password
+      private String password;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public abstract static class MemberResponse {
+
+      private int  memberId;
+      @Email
+      private String email;
+      @Password
+      private String password;
+      @Name
+      private String name;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MemberPostResponse extends MemberResponse {
+      private LocalDateTime createdAt;
+
+      public MemberPostResponse(int memberId, String email, String password, String name, LocalDateTime createdAt) {;
+        this.createdAt = createdAt;
+      }
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MemberPatchResponse extends MemberResponse {
+      private LocalDateTime modifiedAt;
+
+      public MemberPatchResponse(int memberId, String email, String password, String name, LocalDateTime modifiedAt) {
+        super(memberId, email, password, name);
+        this.modifiedAt = modifiedAt;
+      }
+    }
+
+  }
