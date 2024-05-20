a38dc669538aad968db485f1acfcba5a6f0da51d
twoweekhee <myjjoo4758@icloud.com>
2024-05-20 10:10:01 +0900

    ✨ Create concert
    create concert controller, service, repository, entity, dto

--- src/main/java/com/volumeTest/volume/service/ConcertService.java
+++ src/main/java/com/volumeTest/volume/service/ConcertService.java
@@ -1,2 +1,70 @@
-package com.volumeTest.volume.service;public class ConcertService {
+package com.volumeTest.volume.service;
+
+import com.volumeTest.volume.dto.ConcertRequestDto;
+import com.volumeTest.volume.dto.ConcertResponseDto;
+import com.volumeTest.volume.entity.ConcertEntity;
+import com.volumeTest.volume.repository.ConcertRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class ConcertService {
+
+    @Autowired
+    private ConcertRepository concertRepository;
+
+    public ConcertResponseDto createConcert(ConcertRequestDto concertRequestDto) {
+        ConcertEntity concert = concertRepository.save(concertRequestDto.toEntity());
+
+        return  toDto(concert);
+    }
+
+    public ConcertResponseDto findById(Long id) {
+        ConcertEntity concert = concertRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Concert not found with Id : " + id));
+
+        return toDto(concert);
+    }
+
+    public List<ConcertResponseDto> findAll() {
+        List<ConcertEntity> concertList =  concertRepository.findAll();
+        List<ConcertResponseDto> dtoList = concertList.stream()
+                .map(o -> toDto(o))
+                .collect(Collectors.toList());
+        return dtoList;
+    }
+
+    public ConcertResponseDto updateConcert(Long id, ConcertRequestDto concertRequestDto){
+        ConcertEntity foundConcert = concertRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Concert not found with Id : " + id));
+
+        foundConcert.builder()
+                .concertName(concertRequestDto.getConcertName())
+                .concertDate(concertRequestDto.getConcertDate())
+                .build();
+
+        ConcertEntity updateConcert = concertRepository.save(foundConcert);
+
+        return toDto(updateConcert);
+    }
+
+    public void deleteConcert(Long id) {
+        ConcertEntity concert = concertRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Concert not found with Id : " + id));
+
+        concertRepository.delete(concert);
+    }
+
+
+    public ConcertResponseDto toDto(ConcertEntity concert) {
+        return ConcertResponseDto.builder()
+                .concertId(concert.getConcertID())
+                .concertName(concert.getConcertName())
+                .concertDate(concert.getConcertDate())
+                .build();
+    }
 }
