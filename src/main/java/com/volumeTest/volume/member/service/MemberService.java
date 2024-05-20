759a52f174aa57a835c7e1a850bbac97ab130143
Jgone2 <jgoneit@gmail.com>
2024-05-20 14:46:22 +0900

    :sparkles:: KAN-10--Member MemberService Interface 생성

--- src/main/java/com/volumeTest/volume/member/service/MemberService.java
+++ src/main/java/com/volumeTest/volume/member/service/MemberService.java
@@ -1,2 +1,21 @@
-package com.volumeTest.volume.member.service;public interface MemberService {
+package com.volumeTest.volume.member.service;
+
+import com.volumeTest.volume.member.Entity.Member;
+
+public interface MemberService {
+
+    // 회원 생성
+    Member createMember(Member member);
+
+    // 회원 조회
+    Member findMemberByEmail(String email);
+
+    // 회원 수정
+    Member updateMember(Member member, String password);
+
+    // 회원 비밀번호 변경
+    Member updateMemberPassword(Member member, String password);
+
+    // 회원 탈퇴
+    void deleteMember(Member member, String password);
 }
