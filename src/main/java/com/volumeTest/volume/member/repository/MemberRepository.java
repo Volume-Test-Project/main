61a55462e8e144bab03eb3696f628e1e459eb5de
jun <yoseb95@gmail.com>
2024-05-21 15:07:46 +0900

    feat: Member Repository 작성 findByEmail, findAll 메서드 작성

---
+++ src/main/java/com/volumeTest/volume/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.volumeTest.volume.member.repository;
+
+import com.volumeTest.volume.member.entity.Member;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    List<Member> findAll();
+}
