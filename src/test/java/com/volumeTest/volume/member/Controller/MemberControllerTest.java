3c88423e6eae552fe370498a6d2ad54b2f65ed7e
Jgone2 <jgoneit@gmail.com>
2024-05-22 10:43:35 +0900

    ✅: KAN-10--Member Controller Level Test 회원가입 성공 테스트

--- src/test/java/com/volumeTest/volume/member/Controller/MemberControllerTest.java
+++ src/test/java/com/volumeTest/volume/member/Controller/MemberControllerTest.java
@@ -5,7 +5,6 @@ import com.volumeTest.volume.member.dto.MemberDto;
 import com.volumeTest.volume.member.entity.Member;
 import com.volumeTest.volume.member.mapper.MemberMapper;
 import com.volumeTest.volume.member.service.MemberService;
-import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,23 +12,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
-@Transactional
+//@Transactional
 @WebMvcTest(MemberController.class)
 class MemberControllerTest {
 
   @Autowired
   MemberController memberController;
-
 
   @Autowired
   private MockMvc mockMvc;
@@ -42,9 +41,10 @@ class MemberControllerTest {
 
   @Test
   @DisplayName("회원 가입 성공 테스트")
+  @WithMockUser(username = "test", roles = "USER")
   void postMember() throws Exception {
     // given
-    MemberDto.Post post = new MemberDto.Post("test@example.com", "password", "test");
+    MemberDto.Post post = new MemberDto.Post("test@gmail.com","a123456!#", "김테스트");
     Member member = new Member(1, post.getEmail(), post.getPassword(), post.getName());
 
     given(memberMapper.memberPostDtoToMember(any(MemberDto.Post.class))).willReturn(member);
@@ -53,7 +53,8 @@ class MemberControllerTest {
     // when & then
     mockMvc.perform(post("/member/signup")
                     .contentType(MediaType.APPLICATION_JSON)
-                    .content(new ObjectMapper().writeValueAsString(post)))
+                    .content(new ObjectMapper().writeValueAsString(post))
+                    .with(csrf()))
             .andExpect(status().isCreated());
 
     then(memberService).should().createMember(any(Member.class));
