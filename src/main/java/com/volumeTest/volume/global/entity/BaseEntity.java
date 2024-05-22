1f9ed628b6d5b9e0bb09f8e0c7cf952cd537a710
DongDongShin <tlsehdgk87@naver.com>
2024-05-23 00:27:38 +0900

    ✨: KAN-10--Member 엔티티 생성과 수정 date를 표시하는 BaseEntity 생성

---
+++ src/main/java/com/volumeTest/volume/global/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.volumeTest.volume.global.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@MappedSuperclass
+public class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    @Column(updatable = false)
+    private LocalDate updateAt;
+
+
+}
