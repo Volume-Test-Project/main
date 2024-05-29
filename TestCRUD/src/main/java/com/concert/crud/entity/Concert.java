b10f6015a95ca66f5901bd8cb8e33077c9e41ac6
hayeon1ee <hyeon.v29@gmail.com>
2024-05-29 16:14:40 +0900

    ìcon콘서í:sparkles:: í 콘서트엔티티 생ì

---
+++ TestCRUD/src/main/java/com/concert/crud/entity/Concert.java
@@ -0,0 +1,35 @@
+package com.concert.crud.entity;
+/**
+ * 콘서트 Entity
+ * @author 하연
+ */
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity(name="concert")
+@Getter
+//@Setter
+//@Builder
+@NoArgsConstructor
+//@AllArgsConstructor
+@Table(name = "CONCERTS")
+public class Concert {
+    /**
+     * 콘서트 정보
+     * 1. 콘서트 ID - PK
+     * 2. 콘서트 이름
+     * 3. 콘서트 데이터
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int concertId;
+
+    @Column(nullable = false, length = 20)
+    private String concertName;
+
+    @Column(nullable = false)
+    private LocalDate concertDate;
+
+}
