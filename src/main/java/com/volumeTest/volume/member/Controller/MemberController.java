75f9287fa284871ed0982325157710ad1072f924
jun <yoseb95@gmail.com>
2024-06-03 14:08:25 +0900

    feat: MemberController 에 ApiResponseUtil 적용

--- src/main/java/com/volumeTest/volume/member/Controller/MemberController.java
+++ src/main/java/com/volumeTest/volume/member/Controller/MemberController.java
@@ -1,6 +1,10 @@
 package com.volumeTest.volume.member.Controller;
 
+import com.volumeTest.volume.common.util.ApiResponseUtil;
+import com.volumeTest.volume.common.util.ApiResponseUtil.ApiResult;
 import com.volumeTest.volume.member.dto.MemberDto;
+import com.volumeTest.volume.member.dto.MemberDto.MemberPatchResponse;
+import com.volumeTest.volume.member.dto.MemberDto.MemberPostResponse;
 import com.volumeTest.volume.member.entity.Member;
 import com.volumeTest.volume.member.mapper.MemberMapper;
 import com.volumeTest.volume.member.service.MemberService;
@@ -23,40 +27,39 @@ public class MemberController {
 
   // 회원 가입
   @PostMapping("/signup")
-  public ResponseEntity postMember(@RequestBody @Valid MemberDto.Post memberPostDto) {
+  public ApiResult<String> postMember(@RequestBody @Valid MemberDto.Post memberPostDto) {
     memberService.createMember(memberPostDto);
-    return new ResponseEntity<>(HttpStatus.CREATED);
+    return ApiResponseUtil.success("회원가입이 완료되었습니다.");
   }
 
   // 회원조회
   @GetMapping("/info")
-  public ResponseEntity getMember(@AuthenticationPrincipal String email) {
+  public ApiResult<MemberPostResponse> getMember(@AuthenticationPrincipal String email) {
 
     Member findMember = memberService.findMemberByEmail(email);
 
-
-    return ResponseEntity.ok(mapper.memberToMemberPostResponseDto(findMember));
+    return ApiResponseUtil.success(mapper.memberToMemberPostResponseDto(findMember));
   }
 
   // 회원정보 수정
   @PatchMapping("/mypage/update")
-  public ResponseEntity patchMember(@AuthenticationPrincipal String email,
-                                    @Valid @RequestBody MemberDto.Patch memberPatchDto) {
+  public ApiResult<MemberPatchResponse> patchMember(@AuthenticationPrincipal String email,
+                                                    @Valid @RequestBody MemberDto.Patch memberPatchDto) {
 
     Member findMember = memberService.findMemberByEmail(email);
     Member updateMember = memberService.updateMember(findMember, memberPatchDto.getName(), memberPatchDto.getPassword());
 
-    return ResponseEntity.ok(mapper.memberToMemberPatchResponseDto(updateMember));
+    return ApiResponseUtil.success(mapper.memberToMemberPatchResponseDto(updateMember));
   }
 
   // 회원탈퇴
   @DeleteMapping("/mypage/delete")
-  public ResponseEntity deleteMember(@AuthenticationPrincipal String email,
+  public ApiResult<ResponseEntity> deleteMember(@AuthenticationPrincipal String email,
                                         @Valid  @RequestBody MemberDto.CheckPassword checkPasswordDto) {
 
     Member findMember = memberService.findMemberByEmail(email);
     memberService.deleteMember(findMember, checkPasswordDto.getPassword());
 
-    return ResponseEntity.noContent().build();
+    return ApiResponseUtil.success(ResponseEntity.noContent().build());
   }
 }
