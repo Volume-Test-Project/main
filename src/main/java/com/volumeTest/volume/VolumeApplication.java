cc7378214fe0895d876afccd3c703075a879d396
jun <yoseb95@gmail.com>
2024-06-03 17:57:29 +0900

    KAN-21 feat: application.yml 에서 jwt issuer와 secretKey 추출 JwtProperties 객체에 저장

--- src/main/java/com/volumeTest/volume/VolumeApplication.java
+++ src/main/java/com/volumeTest/volume/VolumeApplication.java
@@ -2,9 +2,12 @@ package com.volumeTest.volume;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class VolumeApplication {
 
