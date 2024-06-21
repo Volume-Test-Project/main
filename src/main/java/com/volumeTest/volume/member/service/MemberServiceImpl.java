b2ca55260933d73fc5f7d95aa9ff224cf39d557f
Jgone2 <jgoneit@gmail.com>
2024-06-21 11:31:06 +0900

    :recycle:: refactor: delete login to Dto To Entity and add updateMethod for Jpa Dirty Checking

--- src/main/java/com/volumeTest/volume/member/service/MemberServiceImpl.java
+++ src/main/java/com/volumeTest/volume/member/service/MemberServiceImpl.java
@@ -1,6 +1,5 @@
 package com.volumeTest.volume.member.service;
 
-import com.volumeTest.volume.common.exception.ExceptionStatus;
 import com.volumeTest.volume.common.pattern.Validator.MemberValidator;
 import com.volumeTest.volume.member.dto.*;
 import com.volumeTest.volume.member.entity.Member;
@@ -43,62 +42,49 @@ public class MemberServiceImpl implements MemberService {
   }
 
   @Override
+  @Transactional
   public MemberResponseDto updateMember(String email, MemberUpdateDto memberUpdateDto) {
     // 회원 조회
     Member findMember = memberValidator.findVerifyMemberByEmail(email);
 
-    memberValidator.checkPassword(findMember, memberUpdateDto.getPassword());
+    memberValidator.checkPasswordForMemberUpdateAndDelete(findMember, memberUpdateDto.getPassword());
 
     // 변경할 정보 저장
-    Member updatedMember = memberMapper.memberUpdateDtoToMember(memberUpdateDto);
-
-    // 변경된 정보 저장
-    memberRepository.save(updatedMember);
-
-    MemberUpdateResponseDto memberUpdateResponse = memberMapper.memberToMemberPutResponseDto(updatedMember);
+    findMember.updateMember(memberUpdateDto);
+    MemberResponseDto updatedMember = memberMapper.memberUpdateDtoToMemberResponseDto(memberUpdateDto);
 
     log.info("{}님의 정보가 수정되었습니다. 수정된 정보: 이름={}, 이메일={}", findMember.getName(), updatedMember.getName(), updatedMember.getEmail());
-    return memberUpdateResponse;
+    return updatedMember;
   }
 
+
   @Override
+  @Transactional
   public MemberResponseDto updateMemberPassword(String email, MemberUpdatePasswordDto memberUpdatePasswordDto) {
     Member findMember = memberValidator.findVerifyMemberByEmail(email);
 
     // 새로운 비밀번호와 이전 비밀번호가 같으면 변경하지 않음
-    memberValidator.verifyPrePasswordAndNewPasswordMatch(passwordEncoder.matches(memberUpdatePasswordDto.getPassword(), findMember.getPassword()), ExceptionStatus.MEMBER_PASSWORD_NOT_CHANGE);
+    memberValidator.checkPasswordForMemberUpdatePassword(findMember, memberUpdatePasswordDto.getPassword());
 
     // 변경할 비밀번호 암호화
     String encryptedPassword = passwordEncoder.encode(memberUpdatePasswordDto.getPassword());
 
     // 변경할 암호화된 비밀번호 저장
-    Member updatedMember = rebuildMemberPassword(findMember, encryptedPassword);
+    findMember.updateMemberPassword(encryptedPassword);
 
-    // 변경된 정보 저장
-    updatedMember = memberRepository.save(updatedMember);
-
-    MemberResponseDto updatePasswordResponse = memberMapper.entityToResponse(updatedMember);
+    MemberResponseDto updatePasswordResponse = memberMapper.entityToResponse(findMember);
 
     return updatePasswordResponse;
   }
 
   @Override
+  @Transactional
   public void deleteMember(String email, MemberDeleteDto memberDeleteDto) {
     Member findMember = memberValidator.findVerifyMemberByEmail(email);
     // 패스워드로 검증
-    String encryptedPassword = passwordEncoder.encode(memberDeleteDto.getPassword());
-    memberValidator.checkPassword(findMember, encryptedPassword);
+    memberValidator.checkPasswordForMemberUpdateAndDelete(findMember, memberDeleteDto.getPassword());
 
     log.info("{}님의 정보가 삭제되었습니다.", findMember.getEmail());
     memberRepository.delete(findMember);
-  }
-  
-  private Member rebuildMemberPassword(Member member, String encryptedPassword) {
-    return Member.builder()
-            .memberId(member.getMemberId())
-            .email(member.getEmail())
-            .password(encryptedPassword)
-            .name(member.getName())
-            .build();
   }
 }
