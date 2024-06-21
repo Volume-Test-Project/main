2495dc0d11cb78b3f3fc89cf84772963483a8829
Jgone2 <jgoneit@gmail.com>
2024-06-21 11:28:28 +0900

    :recycle:: refactor: add method memberUpdateDtoToMemberResponseDto and delete no usage Methods

--- src/main/java/com/volumeTest/volume/member/mapper/MemberMapper.java
+++ src/main/java/com/volumeTest/volume/member/mapper/MemberMapper.java
@@ -6,16 +6,8 @@ import com.volumeTest.volume.member.entity.Member;
 public interface MemberMapper {
   // To Entity
   Member memberCreateDtoToMember(MemberCreateDto memberCreateDto, String encryptedPassword);
-  Member memberUpdateDtoToMember(MemberUpdateDto memberUpdateDto);
-
-  // Login
-  MemberLoginDto memberToLoginDto(Member member);
-
-  // Check
-  MemberCheckPasswordDto checkPasswordToMember(MemberCheckPasswordDto checkPasswordDto);
 
   // Response
   MemberResponseDto entityToResponse(Member member);
-  MemberCreateResponseDto memberToMemberPostResponseDto(Member member);
-  MemberUpdateResponseDto memberToMemberPutResponseDto(Member member);
+  MemberResponseDto memberUpdateDtoToMemberResponseDto(MemberUpdateDto memberUpdateDto);
 }
--- src/main/java/com/volumeTest/volume/member/mapper/MemberMapperImpl.java
+++ src/main/java/com/volumeTest/volume/member/mapper/MemberMapperImpl.java
@@ -1,13 +1,9 @@
 package com.volumeTest.volume.member.mapper;
 
-import com.volumeTest.volume.common.pattern.Validator.MemberValidator;
 import com.volumeTest.volume.member.dto.*;
 import com.volumeTest.volume.member.entity.Member;
-import java.time.LocalDateTime;
 import javax.annotation.processing.Generated;
 
-import com.volumeTest.volume.member.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Generated(
@@ -16,11 +12,7 @@ import org.springframework.stereotype.Component;
         comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.7.jar, environment: Java 17.0.9 (Oracle Corporation)"
 )
 @Component
-@RequiredArgsConstructor
 public class MemberMapperImpl implements MemberMapper {
-
-  private final MemberRepository memberRepository;
-  private final MemberValidator memberValidator;
 
   @Override
   public Member memberCreateDtoToMember(MemberCreateDto memberCreateDto, String encryptedPassword) {
@@ -35,45 +27,15 @@ public class MemberMapperImpl implements MemberMapper {
             .build();
   }
 
-
   @Override
-  public Member memberUpdateDtoToMember(MemberUpdateDto memberUpdateDto) {
-    if (memberUpdateDto == null) {
-      return null;
-    }
-    // 회원 조회
-    Member findMember = memberValidator.findVerifyMemberByEmail(memberUpdateDto.getEmail());
-
-    // Member 객체를 복사하고 변경할 필드만 수정
-    return Member.builder()
-            .memberId(findMember.getMemberId()) // 기존 ID 유지
-            .email(findMember.getEmail())       // 기존 이메일 유지 (변경 불가)
-            .password(findMember.getPassword()) // 새 비밀번호 설정
-            .name(memberUpdateDto.getName())    // 새 이름 설정
-            .build();
-  }
-
-  @Override
-  public MemberLoginDto memberToLoginDto(Member member) {
-    if ( member == null ) {
+  public MemberResponseDto memberUpdateDtoToMemberResponseDto(MemberUpdateDto memberUpdateDto)  {
+    if ( memberUpdateDto == null ) {
       return null;
     }
 
-    MemberLoginDto login = new MemberLoginDto();
+    MemberResponseDto memberResponse = new MemberResponseDto(memberUpdateDto);
 
-    return login;
-  }
-
-  @Override
-  public MemberCheckPasswordDto checkPasswordToMember(MemberCheckPasswordDto checkPasswordDto) {
-    if ( checkPasswordDto == null ) {
-      return null;
-    }
-    String password = null;
-    password = checkPasswordDto.getPassword();
-    MemberCheckPasswordDto checkPassword = new MemberCheckPasswordDto(password);
-
-    return checkPassword;
+    return memberResponse;
   }
 
   @Override
@@ -82,48 +44,7 @@ public class MemberMapperImpl implements MemberMapper {
       return null;
     }
 
-    int memberId = 0;
-    String email = null;
-    String password = null;
-    String name = null;
-    LocalDateTime createdAt = null;
-    LocalDateTime modifiedAt = null;
-
     MemberResponseDto memberResponse = new MemberResponseDto(member);
     return memberResponse;
-  }
-
-  @Override
-  public MemberCreateResponseDto memberToMemberPostResponseDto(Member member) {
-    if ( member == null ) {
-      return null;
-    }
-
-    int memberId = 0;
-    String email = null;
-    String password = null;
-    String name = null;
-    LocalDateTime createdAt = null;
-
-    MemberCreateResponseDto memberPostResponse = new MemberCreateResponseDto(member, createdAt );
-
-    return memberPostResponse;
-  }
-
-  @Override
-  public MemberUpdateResponseDto memberToMemberPutResponseDto(Member member) {
-    if ( member == null ) {
-      return null;
-    }
-
-    int memberId = 0;
-    String email = null;
-    String password = null;
-    String name = null;
-    LocalDateTime modifiedAt = null;
-
-    MemberUpdateResponseDto memberPutResponse = new MemberUpdateResponseDto(member, modifiedAt );
-
-    return memberPutResponse;
   }
 }
