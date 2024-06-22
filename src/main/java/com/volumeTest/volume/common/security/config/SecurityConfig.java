7f0f2696cfdf3dcabf207bc4c9583ef155f45b7c
jun <yoseb95@gmail.com>
2024-06-22 18:46:29 +0900

    KAN-21 feat: SecurityConfig 설정 방식 업데이트 security-config:6.2.4 에서 권장하는 함수형 방식으로 업데이트

--- src/main/java/com/volumeTest/volume/common/security/config/SecurityConfig.java
+++ src/main/java/com/volumeTest/volume/common/security/config/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.volumeTest.volume.common.security.config;
 
+import com.volumeTest.volume.common.security.filter.TokenAuthenticationFilter;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -18,27 +26,26 @@ import java.util.List;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
+@AllArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+
+  TokenAuthenticationFilter tokenAuthenticationFilter;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
     http
-            .csrf()
-            .ignoringRequestMatchers(PathRequest.toH2Console())
-            .and()
-            .headers()
-            .frameOptions().sameOrigin()
-            .and()
+//            .csrf((csrf) -> csrf.ignoringRequestMatchers(PathRequest.toH2Console())) // H2 콘솔 접근 허용
+            .csrf(AbstractHttpConfigurer::disable) // CSRF 보안 토큰 disable
+            .headers((headers) -> headers.frameOptions(FrameOptionsConfig::sameOrigin))
+
             .cors(withDefaults())
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .formLogin().disable()
-            .httpBasic().disable()
-            .exceptionHandling()
-            .and()
+            .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+//            .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
             .authorizeHttpRequests(authorize ->
               authorize
                 .requestMatchers(PathRequest.toH2Console()).permitAll() // H2 콘솔 접근 허용
@@ -57,6 +64,7 @@ public class SecurityConfig {
 
     corsConfiguration.setAllowCredentials(true);
     corsConfiguration.addAllowedOrigin("http://localhost:3000"); // 허용할 Origin 추가
+    corsConfiguration.addAllowedOrigin("http://localhost:8080"); // 허용할 Origin 추가
     corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
     corsConfiguration.addAllowedHeader("Authorization");
     corsConfiguration.addAllowedHeader("Content-Type");
